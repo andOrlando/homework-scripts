3343789df441ca3b0227619c703dd8274c9cf15b
andOrlando <bennettgillig@gmail.com>
2024-04-03 13:31:02 -0400

    Makes classquestion more verbose

--- classquestion.js
+++ classquestion.js
@@ -1,6 +1,6 @@
 setInterval(() => {
 	const ansbtn = document.querySelector(".ansbtn")
 	const submitbtn = document.getElementById("submitbtn")
-	if (ansbtn) ansbtn.click()
-	if (submitbtn) submitbtn.click()
+	if (ansbtn) { console.log("clicked A"); ansbtn.click() }
+	if (submitbtn) { console.log("clicked submit"); submitbtn.click() }
 }, 10000)
